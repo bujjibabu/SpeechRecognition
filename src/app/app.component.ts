17f1c471d3f3ead8da8499611dbcb315c307992d
BujjibabuBalaga <Bujjibabu.Balaga@iddinkgroup.com>
2019-10-18 18:23:04 +0530

    initial commit.

---
+++ src/app/app.component.ts
@@ -0,0 +1,182 @@
+import { Component, OnInit, AfterViewInit, NgZone, ViewChild } from '@angular/core';
+import { Observable } from 'rxjs';
+import {FormBuilder, FormGroup} from '@angular/forms';
+
+declare let window;
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent implements OnInit, AfterViewInit  {
+  @ViewChild('mySelect', {static: false}) mySelect: any;
+  speechRecognition: any;
+  recognition: any;
+  noSupport = false;
+  noteTextarea: any = '';
+  notes: any;
+  options: FormGroup;
+  title: any;
+  reason: any;
+  type: any;
+  leaveObj: any;
+  verifySentance: any;
+  language = 'en-US';
+
+  constructor(private zone: NgZone, fb: FormBuilder) {
+    this.options = fb.group({});
+    this.notes = [];
+    try {
+      this.speechRecognition = window.SpeechRecognition || window['webkitSpeechRecognition'];
+      this.recognition = new this.speechRecognition();
+    } catch (e) {
+      console.error(e);
+      this.noSupport = true;
+    }
+  }
+
+  ngAfterViewInit() {
+   // this.mySelect.open();
+  }
+
+  ngOnInit() {
+
+    /*-----------------------------
+          Voice Recognition
+    ------------------------------*/
+
+    // If false, the recording will stop after a few seconds of silence.
+    // When true, the silence period is longer (about 15 seconds),
+    // allowing us to keep recording even when the user pauses.
+    this.recognition.continuous = true;
+    this.recognition.lang = 'nl-nl';
+    // This block is called every time the Speech APi captures a line.
+
+    this.recognition.onstart = function () {
+      // instructions.text('Voice recognition activated. Try speaking into the microphone.');
+    }
+
+    this.recognition.onspeechend = function () {
+      // instructions.text('You were quiet for a while so voice recognition turned itself off.');
+    }
+
+    this.recognition.onerror = function (event) {
+      if (event.error == 'no-speech') {
+        // instructions.text('No speech was detected. Try again.');
+      };
+    }
+  }
+
+  titleStartRecognition() {
+    this.record().subscribe((value) => {
+      this.title = value;
+      this.recognition.stop();
+      console.log(value);
+    },
+      (err) => {
+        this.recognition.stop();
+        console.log(err);
+        if (err.error === 'no-speech') {
+          console.log('--restatring service--');
+        }
+      },
+      () => {
+        console.log('--complete--');
+      });
+    this.recognition.start();
+  }
+
+  reasonStartRecognition() {
+    this.record().subscribe((value) => {
+      this.reason = value;
+      this.recognition.stop();
+      console.log(value);
+    },
+      (err) => {
+        this.recognition.stop();
+        console.log(err);
+        if (err.error === 'no-speech') {
+          console.log('--restatring service--');
+        }
+      },
+      () => {
+        console.log('--complete--');
+      });
+    this.recognition.start();
+  }
+
+  typeStartRecognition() {
+    this.record().subscribe((value) => {
+      this.type = value;
+      this.mySelect.close();
+      this.recognition.stop();
+      console.log(value);
+    },
+      (err) => {
+        this.recognition.stop();
+        console.log(err);
+        if (err.error === 'no-speech') {
+          console.log('--restatring service--');
+        }
+      },
+      () => {
+        console.log('--complete--');
+      });
+    this.recognition.start();
+  }
+
+  submit() {
+    this.recognition.stop();
+    // need to do Http call
+    console.log(this.leaveObj);
+  }
+
+  verify() {
+    this.recognition.stop();
+    this.leaveObj = {
+      title: 'your title ' + this.title,
+      reason: 'your reason ' + this.reason,
+      type: 'your type ' + this.type
+    };
+
+    this.verifySentance = 'your title ' + this.title  + '  your reason ' + this.reason + 'your type ' + this.type;
+    this.readOutLoud(this.verifySentance);
+    console.log(this.leaveObj);
+  }
+
+  record(): Observable<string> {
+    return Observable.create(observer => {
+      this.recognition.onresult = event => {
+        let term: string = "";
+        let current = event.resultIndex;
+        // Get a transcript of what was said.
+        let transcript = event.results[current][0].transcript;
+        // Add the current transcript to the contents of our Note.
+        // There is a weird bug on mobile, where everything is repeated twice.
+        // There is no official solution so far so we have to handle an edge case.
+        let mobileRepeatBug = (current == 1 && transcript == event.results[0][0].transcript);
+        if (!mobileRepeatBug) {
+          this.noteTextarea = transcript;
+          term = this.noteTextarea;
+        }
+        this.zone.run(() => {
+          observer.next(term);
+        });
+      };
+    });
+  }
+
+  /*-----------------------------
+          Speech Synthesis
+  ------------------------------*/
+  readOutLoud(message) {
+    const speech = new SpeechSynthesisUtterance();
+    speech.text = message;
+    speech.volume = 1;
+    speech.rate = 1;
+    speech.pitch = 1;
+    window.speechSynthesis.speak(speech);
+  }
+
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {DemoMaterialModule} from './material-module';
+import {MatNativeDateModule} from '@angular/material/core';
+import { AppComponent } from './app.component';
+import { SpeechRecognitionService } from './speech-recognition.service';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    DemoMaterialModule,
+    ReactiveFormsModule,
+    MatNativeDateModule
+  ],
+  providers: [SpeechRecognitionService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
